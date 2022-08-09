eb550f3efd09f6dc5880c12d2755cd66dc12ca29
HatterJS <hatter3ds@gmail.com>
2022-08-09 23:05:31 +0300

    add gallery item selectors

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import NavBar from './components/NavBar';
 import imageSrc01 from './img/pictures/img_01.jpg';
 import imageSrc02 from './img/pictures/img_02.jpg';
 import imageSrc03 from './img/pictures/img_03.jpg';
+import imageSrc04 from './img/pictures/img_04.jpg';
+import imageSrc05 from './img/pictures/img_05.jpg';
 
 function App() {
   return (
@@ -20,10 +22,17 @@ function App() {
           <PhotoItem itemsImageSrc={imageSrc01} itemsTitle='Carpathians' itemsDescription='Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores repudiandae neque dicta repellendus reiciendis magni vitae animi, asperiores doloremque illo ullam, sunt odit assumenda! Hic eveniet nemo iste numquam odit! Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores repudiandae neque dicta repellendus reiciendis magni vitae animi, asperiores doloremque illo ullam, sunt odit assumenda! Hic eveniet nemo iste numquam odit!' />
           <PhotoItem itemsImageSrc={imageSrc02} itemsTitle='Bear' itemsDescription='Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores repudiandae neque dicta repellendus reiciendis magni vitae animi, asperiores doloremque illo ullam, sunt odit assumenda! Hic eveniet nemo iste numquam odit!' />
           <PhotoItem itemsImageSrc={imageSrc03} itemsTitle='Lake' itemsDescription='Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores repudiandae neque dicta repellendus reiciendis magni vitae animi, asperiores doloremque illo ullam, sunt odit assumenda! Hic eveniet nemo iste numquam odit!' />
+          <PhotoItem itemsImageSrc={imageSrc04} itemsTitle='Lake' itemsDescription='Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores repudiandae neque dicta repellendus reiciendis magni vitae animi, asperiores doloremque illo ullam, sunt odit assumenda! Hic eveniet nemo iste numquam odit!' />
+          <PhotoItem itemsImageSrc={imageSrc05} itemsTitle='Lake' itemsDescription='Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores repudiandae neque dicta repellendus reiciendis magni vitae animi, asperiores doloremque illo ullam, sunt odit assumenda! Hic eveniet nemo iste numquam odit!' />
           <PhotoItem />
           <PhotoItem />
           <PhotoItem />
-          <div style={{height: '500px'}}></div>
+          <PhotoItem />
+          <PhotoItem />
+          <PhotoItem />
+          <PhotoItem />
+          <PhotoItem />
+          <PhotoItem />
         </div>
       </body>
     </div>
--- src/components/PhotoItem.js
+++ src/components/PhotoItem.js
@@ -4,17 +4,30 @@ import '../css/photoItem.css';
 class PhotoItem extends React.Component {
     handleUnfold = (e) => {
         const photoItems = document.querySelectorAll('.photoItem');
-        for (let i=0; i<photoItems.length; i++) {
-            photoItems[i].classList.remove('active');
+        if (e.target.classList.contains('photoItem__rightArea')) {   
+            for (let i=0; i<photoItems.length; i++) {
+                photoItems[i].classList.remove('active');
+            }
+            e.currentTarget.nextSibling.classList.add('active');
+        } else if (e.target.classList.contains('photoItem__leftArea')) {   
+            for (let i=0; i<photoItems.length; i++) {
+                photoItems[i].classList.remove('active');
+            }
+            e.currentTarget.previousSibling.classList.add('active');
+        } else {   
+            for (let i=0; i<photoItems.length; i++) {
+                photoItems[i].classList.remove('active');
+            }
+            e.currentTarget.classList.add('active');            
         }
-        e.currentTarget.classList.add('active');
-        console.log(e.currentTarget.classList);
-        console.log(photoItems);
+        console.log(e.currentTarget.nextSibling);  
     }
     render () {
         return (
             <div className="photoItem" onClick={this.handleUnfold}>
                 <div className="photoItem__Content" style={{backgroundImage: `url(${this.props.itemsImageSrc})`}}>
+                    <div className="photoItem__leftArea"><div className="photoItem__leftArrow"></div></div>
+                    <div className="photoItem__rightArea"><div className="photoItem__rightArrow"></div></div>
                     <div className="photoItem__lightShade"></div>
                     <div className="photoItem__darkShade"></div>
                 </div>
